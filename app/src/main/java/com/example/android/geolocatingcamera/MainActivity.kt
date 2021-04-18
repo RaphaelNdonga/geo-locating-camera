2f0893c22e76f6c4ed693efb152cf4db5f96bc95
Raphael Ndonga <nkibi53@gmail.com>
2021-04-18 17:53:46 +0300

    obtains photo thumbnail through intents and extras

--- app/src/main/java/com/example/android/geolocatingcamera/MainActivity.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.android.geolocatingcamera
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import com.example.android.geolocatingcamera.databinding.ActivityMainBinding
+
+const val REQUEST_IMAGE_CAPTURE = 1
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            takePictureIntent()
+        }
+    }
+
+    fun takePictureIntent(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try{
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }catch (e:ActivityNotFoundException){
+            Toast.makeText(this,getString(R.string.camera_not_found),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageView.visibility = View.VISIBLE
+            binding.imageView.setImageBitmap(imageBitmap)
+        }
     }
 }
