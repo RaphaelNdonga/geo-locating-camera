42c83139cc1ffc2f62ed01216a5c73b3ecb2a42c
Raphael Ndonga <nkibi53@gmail.com>
2021-04-19 17:03:47 +0300

    photo is saved to a file

--- app/src/main/java/com/example/android/geolocatingcamera/MainActivity.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/MainActivity.kt
@@ -3,17 +3,27 @@ package com.example.android.geolocatingcamera
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.example.android.geolocatingcamera.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val REQUEST_IMAGE_CAPTURE = 1
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var currentPhotoPath: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,21 +34,73 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun takePictureIntent(){
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try{
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
-        }catch (e:ActivityNotFoundException){
-            Toast.makeText(this,getString(R.string.camera_not_found),Toast.LENGTH_LONG).show()
+    private fun createImageFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmSS").format(Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        return File.createTempFile("JPEG_$timeStamp", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun takePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile = try {
+                    createImageFile()
+                } catch (e: IOException) {
+                    Toast.makeText(this, "Failed to create image ", Toast.LENGTH_LONG).show()
+                    null
+                }
+                photoFile?.let {
+                    val photoUri =
+                        FileProvider.getUriForFile(
+                            this,
+                            "com.example.android.fileprovider",
+                            it
+                        )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    private fun setPic() {
+        val targetH = binding.imageView.height
+        val targetW = binding.imageView.width
+
+        Log.i("MainActivity","imageViewHeight is $targetH")
+        Log.i("MainActivity","imageViewWidth is $targetW")
+
+        val bmOptions = BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+
+            BitmapFactory.decodeFile(currentPhotoPath, this)
+
+            val photoH = outHeight
+            val photoW = outWidth
+
+            Log.i("MainActivity","decodedHeight is $photoH")
+            Log.i("MainActivity","decodedWidth is $photoW")
+
+
+            val scaleFactor = Math.max(1, Math.min(photoW/ targetW, photoH / targetH))
+
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inPurgeable = true
+        }
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions).let {
+            binding.imageView.setImageBitmap(it)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.imageView.visibility = View.VISIBLE
-            binding.imageView.setImageBitmap(imageBitmap)
+            setPic()
         }
     }
 }
