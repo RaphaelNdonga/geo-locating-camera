7d307ed0a21c2c5b01fbdb1c58e2b1902047f984
Raphael Ndonga <nkibi53@gmail.com>
2021-04-26 20:11:56 +0300

    I've never been this bothered by code. firebase functions (that create admin accounts and regular accounts) have been created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,57 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.addAdminCourseId = functions.https.onCall((data, context)=>{
+  functions.logger.info("on call has been called", {structuredData: true});
+  const email = data.email;
+  const departmentId = data.departmentId;
+  return setAdminCourseId(email, departmentId).then(()=>{
+    return {
+      result: `Request fulfilled! ${email} is now an administrator`,
+    };
+  });
+});
+/**
+     * Processing input parameters
+     * @param {string} email
+     * @param {string} departmentId
+     */
+async function setAdminCourseId(email:string, departmentId:string)
+  : Promise<void> {
+  functions.logger.info(`The email is ${email}`);
+  const user = await admin.auth().getUserByEmail(email);
+  functions.logger.info(
+      `setting ${email} as administrator with course id ${
+        departmentId
+      }`, {structuredData: true});
+  return admin.auth().setCustomUserClaims(user.uid, {
+    admin: true,
+    departmentId: departmentId,
+  });
+}
+
+exports.addCourseId = functions.https.onCall((data, context)=>{
+  const departmentId = data.departmentId;
+  const email = data.email;
+  return setCourseId(email, departmentId).then(()=>{
+    return {
+      result: `Request fulfilled! Group id ${departmentId} set`,
+    };
+  });
+});
+
+/**
+     * Processing output parameters
+     * @param {string} email
+     * @param {string} departmentId
+     */
+async function setCourseId(email:string, departmentId:string) {
+  const user = await admin.auth().getUserByEmail(email);
+  functions.logger.info(
+      `setting the course id to ${departmentId}`, {structuredData: true});
+  return admin.auth().setCustomUserClaims(user.uid, {
+    departmentId: departmentId,
+  });
+}
