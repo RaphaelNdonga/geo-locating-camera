1f3d12206404de18bae307d45d20ae8e3cc7da41
Raphael Ndonga <nkibi53@gmail.com>
2021-04-26 20:10:32 +0300

    SignUpViewModel.kt connected to firebase functions server

--- app/src/main/java/com/example/android/geolocatingcamera/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/signup/SignUpViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.ViewModel
 import com.example.android.geolocatingcamera.UserType
 import com.example.android.geolocatingcamera.util.Event
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.ktx.Firebase
 
 class SignUpViewModel(private val userType: UserType) : ViewModel() {
     private val auth = Firebase.auth
+    private val functions = FirebaseFunctions.getInstance()
 
     private val _snackBarText = MutableLiveData<Event<String>>()
     val snackBarText: LiveData<Event<String>> = _snackBarText
@@ -22,7 +24,14 @@ class SignUpViewModel(private val userType: UserType) : ViewModel() {
     fun createUser(email: String, password: String, departmentId: String) {
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                navigateToLogin()
+                when(userType){
+                    UserType.ADMIN ->{
+                        createAdminUser(email,departmentId)
+                    }
+                    UserType.REGULAR -> {
+                        createRegularUser(email,departmentId)
+                    }
+                }
             } else {
                 _snackBarText.value = Event("Failed to create user. Please try again")
                 Log.d("SignUpViewModel", task.exception.toString())
@@ -32,5 +41,18 @@ class SignUpViewModel(private val userType: UserType) : ViewModel() {
 
     private fun navigateToLogin() {
         _loginNavigator.value = Event(Unit)
+    }
+
+    private fun createAdminUser(email:String,departmentId: String){
+        val data = hashMapOf("email" to email,"departmentId" to departmentId)
+        functions.getHttpsCallable("addAdminCourseId").call(data).continueWith {
+            navigateToLogin()
+        }
+    }
+    private fun createRegularUser(email: String,departmentId: String){
+        val data = hashMapOf("email" to email,"departmentId" to departmentId)
+        functions.getHttpsCallable("addCourseId").call(data).continueWith {
+            navigateToLogin()
+        }
     }
 }
