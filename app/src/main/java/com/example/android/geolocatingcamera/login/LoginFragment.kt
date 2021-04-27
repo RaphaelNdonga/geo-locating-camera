395e22b1a698f47afb5b8aec61c4733f460b6d08
Raphael Ndonga <nkibi53@gmail.com>
2021-04-27 08:54:32 +0300

    sign in functionality added and id tokens obtained

--- app/src/main/java/com/example/android/geolocatingcamera/login/LoginFragment.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/login/LoginFragment.kt
@@ -1,14 +1,29 @@
 package com.example.android.geolocatingcamera.login
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.example.android.geolocatingcamera.MainActivity
+import com.example.android.geolocatingcamera.R
 import com.example.android.geolocatingcamera.UserType
 import com.example.android.geolocatingcamera.databinding.LoginFragmentBinding
+import com.example.android.geolocatingcamera.util.DEPARTMENT_ID
+import com.example.android.geolocatingcamera.util.IS_ADMIN
+import com.example.android.geolocatingcamera.util.isValidEmail
+import com.example.android.geolocatingcamera.util.sharedPrefFile
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
@@ -20,11 +35,21 @@ class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
 
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        sharedPreferences =
+            requireActivity().getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+
+        auth = Firebase.auth
         binding = LoginFragmentBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
 
         val adminSignUp = binding.adminSignUpBtn
         val regularSignUp = binding.regularSignUpBtn
@@ -45,13 +70,56 @@ class LoginFragment : Fragment() {
             )
         }
 
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emailEditTxt.text.toString()
+            val password = binding.passwordEditTxt.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Snackbar.make(requireView(), R.string.fill_blanks, Snackbar.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                getIdToken()
+            }.addOnFailureListener {
+                val exception = it.message
+                Toast.makeText(context, exception, Toast.LENGTH_LONG).show()
+            }
+        }
+
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onStart() {
+        super.onStart()
+        val user = auth.currentUser
+        if (user != null){
+            goToMainActivity()
+        }
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(context, MainActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
+    }
+
+    private fun getIdToken() {
+        val user = auth.currentUser
+        user?.getIdToken(false)?.addOnSuccessListener { result ->
+            val isAdmin = result.claims["admin"] as Boolean?
+            val departmentId = result.claims["departmentId"] as String?
+
+            departmentId?.let {
+                sharedPreferences.edit().putString(DEPARTMENT_ID, it).apply()
+            }
+            isAdmin?.let {
+                sharedPreferences.edit().putBoolean(IS_ADMIN, it).apply()
+            }
+            goToMainActivity()
+        }?.addOnFailureListener {
+            Toast.makeText(context, it.message, Toast.LENGTH_LONG).show()
+        }
     }
 
 }
