b9f4ac010109f6fe0ec693b5cd33874a3a73c90d
Raphael Ndonga <nkibi53@gmail.com>
2021-04-27 08:51:58 +0300

    constants for shared preferences

---
+++ app/src/main/java/com/example/android/geolocatingcamera/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.android.geolocatingcamera.util
+
+const val sharedPrefFile = "com.example.android.geolocatingcamera"
+const val DEPARTMENT_ID = "departmentId"
+const val IS_ADMIN = "isAdmin"
