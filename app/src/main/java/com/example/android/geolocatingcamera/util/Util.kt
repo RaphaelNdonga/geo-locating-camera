af08fb35c4618516d8ae295ad9899472a6870870
Raphael Ndonga <nkibi53@gmail.com>
2021-04-27 08:53:01 +0300

    method that checks whether the department id is a valid messaging topic

--- app/src/main/java/com/example/android/geolocatingcamera/util/Util.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/util/Util.kt
@@ -4,7 +4,7 @@ import android.util.Patterns
 
 fun String.containsWhiteSpace() = contains("//s".toRegex())
 fun String?.isValidEmail() = !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
-fun String.containsSpecialCharacters() = contains("[^a-zA-Z0-9-_.~%]".toRegex())
+fun String.isValidMessagingTopic() = contains("[^a-zA-Z0-9-_.~%]".toRegex())
 
 
 
