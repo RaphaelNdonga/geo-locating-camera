641a76f390ef1cbc435d609e2b8404a83468ce37
Raphael Ndonga <nkibi53@gmail.com>
2021-04-27 09:47:21 +0300

    constants moved from main activity to this util file

--- app/src/main/java/com/example/android/geolocatingcamera/util/Constants.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/util/Constants.kt
@@ -3,3 +3,6 @@ package com.example.android.geolocatingcamera.util
 const val sharedPrefFile = "com.example.android.geolocatingcamera"
 const val DEPARTMENT_ID = "departmentId"
 const val IS_ADMIN = "isAdmin"
+const val REQUEST_IMAGE_CAPTURE = 1
+const val REQUEST_LOCATION_PERMISSION = 2
+const val REQUEST_CHECK_SETTINGS = 3
