cb3707b6f6c231c3b55031254f18336267f9c430
Raphael Ndonga <nkibi53@gmail.com>
2021-04-27 18:24:40 +0300

    drawer layout connected to the toolbar and navController. For the toolbar to work together with the drawer layout, it needs to connect to an appbarconfiguration and navController. An appbarconfiguration obtains the navGraph and navController

--- app/src/main/java/com/example/android/geolocatingcamera/MainActivity.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/MainActivity.kt
@@ -2,16 +2,34 @@ package com.example.android.geolocatingcamera
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.android.geolocatingcamera.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var customToolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        customToolbar = findViewById(R.id.customToolbar)
+        setSupportActionBar(customToolbar)
+        navController = findNavController(R.id.main_nav_host_fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        customToolbar.setupWithNavController(navController,appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navigateUp(navController, drawerLayout)
     }
 }
