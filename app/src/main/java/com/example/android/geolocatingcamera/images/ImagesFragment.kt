3fdd2bfaf9e36841fbab62cfc51efae88e6dce97
Raphael Ndonga <nkibi53@gmail.com>
2021-04-28 17:10:39 +0300

    ImagesFragment.kt,viewmodel and viewmodel factory have been created to facilitate the display of images taken by the department

--- app/src/main/java/com/example/android/geolocatingcamera/images/ImagesFragment.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/images/ImagesFragment.kt
@@ -5,17 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.geolocatingcamera.databinding.ImagesFragmentBinding
+import com.google.firebase.firestore.ListenerRegistration
 
 class ImagesFragment : Fragment() {
     private lateinit var binding: ImagesFragmentBinding
+    private lateinit var viewModel: ImagesViewModel
+    private lateinit var snapshotListener:ListenerRegistration
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = ImagesFragmentBinding.inflate(layoutInflater)
+        val app = requireActivity().application
+        viewModel =
+            ViewModelProvider(this, ImagesViewModelFactory(app)).get(ImagesViewModel::class.java)
+
+        val adapter = ImagesAdapter()
+        viewModel.geoLocatingData.observe(viewLifecycleOwner,{
+            adapter.submitList(it)
+        })
+        binding.imagesRecyclerView.adapter = adapter
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        snapshotListener = viewModel.addSnapshotListener()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        snapshotListener.remove()
     }
 }
