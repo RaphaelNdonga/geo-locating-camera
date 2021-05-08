9fee8a7ebcf41fe19e79207b595fd3e2148ed368
Raphael Ndonga <nkibi53@gmail.com>
2021-05-08 10:21:02 +0300

    Parcelized to transport it across fragments effectively

--- app/src/main/java/com/example/android/geolocatingcamera/GeoLocatingData.kt
+++ app/src/main/java/com/example/android/geolocatingcamera/GeoLocatingData.kt
@@ -1,10 +1,13 @@
 package com.example.android.geolocatingcamera
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
+@Parcelize
 data class GeoLocatingData (
     val id:String = UUID.randomUUID().toString(),
     var downloadUri:String = "",
     var location:String = "",
     var timeStamp:Long = 0L
-)
+):Parcelable
